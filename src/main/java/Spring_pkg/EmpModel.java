a5e40248041afdb1d1e60c82e1182ee775a0b7b1
Dolly <dollyup8july@gmail.com>
2018-03-31 16:33:22 +0530

    cmit1

---
+++ src/main/java/Spring_pkg/EmpModel.java
@@ -0,0 +1,19 @@
+package Spring_pkg;
+
+public class EmpModel {
+String name,email;
+public String getName() {
+    return this.name;
+}
+
+public void setName(String name) {
+    this.name = name;
+}
+public String getEmail() {
+    return this.email;
+}
+
+public void setEmail(String email) {
+    this.email = email;
+}
+}
--- src/main/java/spirng_project/Spring_File.java
+++ src/main/java/spirng_project/Spring_File.java
@@ -4,10 +4,13 @@ import java.util.HashMap;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import Spring_pkg.EmpModel;
 
 @RestController
 public class Spring_File {
@@ -26,6 +29,13 @@ public class Spring_File {
 		hm.put("status","success");
 		hm.put("file","spring");
 		return hm;
+			}
+	@RequestMapping(value="/snd",method=RequestMethod.POST)
+	public @ResponseBody HashMap<String,Object> maps(@RequestBody EmpModel mod)
+	{
+		HashMap<String,Object> hm=new HashMap<String,Object>();	   
+		hm.put("Email",mod.getEmail());
+		System.out.println(mod.getName());
+		return hm;
 	}
-
 }
